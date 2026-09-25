c6759216966ec2546bad7e1b8af58163e163a83a
Laroxyss <lomonosov_18@mail.ru>
2026-09-25 21:25:21 +0500

    сделал компоненты SixCities и App

---
+++ src/components/app/app.tsx
@@ -0,0 +1,7 @@
+import SixCities from "../../pages/six-cities/six-cities";
+
+function App(): JSX.Element {
+  return <SixCities />;
+}
+
+export default App;
---
+++ src/pages/six-cities/six-cities.tsx
@@ -0,0 +1,84 @@
+import PlaceCard, { PlaceCardType } from '../../components/place-card/place-card';
+
+function SixCities(): JSX.Element {
+  return (
+    <section>
+      <h1 className="visually-hidden">Cities</h1>
+      <div className="tabs">
+        <section className="locations container">
+          <ul className="locations__list tabs__list">
+            <li className="locations__item">
+              <a className="locations__item-link tabs__item" href="#">
+                <span>Paris</span>
+              </a>
+            </li>
+            <li className="locations__item">
+              <a className="locations__item-link tabs__item" href="#">
+                <span>Cologne</span>
+              </a>
+            </li>
+            <li className="locations__item">
+              <a className="locations__item-link tabs__item" href="#">
+                <span>Brussels</span>
+              </a>
+            </li>
+            <li className="locations__item">
+              <a className="locations__item-link tabs__item tabs__item--active">
+                <span>Amsterdam</span>
+              </a>
+            </li>
+            <li className="locations__item">
+              <a className="locations__item-link tabs__item" href="#">
+                <span>Hamburg</span>
+              </a>
+            </li>
+            <li className="locations__item">
+              <a className="locations__item-link tabs__item" href="#">
+                <span>Dusseldorf</span>
+              </a>
+            </li>
+          </ul>
+        </section>
+      </div>
+      <div className="cities">
+        <div className="cities__places-container container">
+          <section className="cities__places places">
+            <h2 className="visually-hidden">Places</h2>
+            <b className="places__found">312 places to stay in Amsterdam</b>
+            <form className="places__sorting" action="#" method="get">
+              <span className="places__sorting-caption">Sort by</span>
+              <span className="places__sorting-type" tabIndex={0}>
+                Popular
+                <svg className="places__sorting-arrow" width="7" height="4">
+                  <use xlinkHref="#icon-arrow-select"></use>
+                </svg>
+              </span>
+              <ul className="places__options places__options--custom places__options--opened">
+                <li className="places__option places__option--active" tabIndex={0}>Popular</li>
+                <li className="places__option" tabIndex={0}>Price: low to high</li>
+                <li className="places__option" tabIndex={0}>Price: high to low</li>
+                <li className="places__option" tabIndex={0}>Top rated first</li>
+              </ul>
+            </form>
+            <div className="cities__places-list places__list tabs__content">
+              <PlaceCard title="Beautiful &amp; luxurious apartment at great location" type={PlaceCardType.Apartment} img="img/apartment-01.jpg" cost={120} rating={80} isPremium={true} />
+
+              <PlaceCard title="Wood and stone place" type={PlaceCardType.Room} img="img/room.jpg" cost={80} rating={80} isBookmarked={true} />
+
+              <PlaceCard title="Canal View Prinsengracht" type={PlaceCardType.Apartment} img="img/apartment-02.jpg" cost={132} rating={80} />
+
+              <PlaceCard title="Nice, cozy, warm big bed apartment" type={PlaceCardType.Apartment} img="img/apartment-03.jpg" cost={180} rating={100} isPremium={true} />
+
+              <PlaceCard title="Wood and stone place" type={PlaceCardType.Room} img="img/room.jpg" cost={80} rating={80} isBookmarked={true} />
+            </div>
+          </section>
+          <div className="cities__right-section">
+            <section className="cities__map map"></section>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default SixCities;
